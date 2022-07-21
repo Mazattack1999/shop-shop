e8e457e97999d7c933907e2d47c4f35ad7b33b5a
micah_z <micah.a.zahn@gmail.com>
2022-07-21 17:29:28 -0500

    add IndexedDB functionality

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -14,7 +14,6 @@ const StoreProvider = ({value = [], ...props}) => {
         currentCategory: '',
     })
     // use this to confirm it works!
-    console.log(state);
     return <Provider value={[state, dispatch]} {...props} />;
 }
 
